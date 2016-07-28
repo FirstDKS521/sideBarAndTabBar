eed586b58c3523f7415bb0902feed93b6d1a7eb7
duks <duks@robooot.com>
2016-07-28 19:36:29 +0800

    Initial Commit

---
+++ ZheShang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZheShang
+//
+//  Created by aDu on 16/7/28.
+//  Copyright © 2016年 DuKaiShun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZheShang/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZheShang
+//
+//  Created by aDu on 16/7/28.
+//  Copyright © 2016年 DuKaiShun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
